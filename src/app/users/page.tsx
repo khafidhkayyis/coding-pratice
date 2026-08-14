f5774b7f97fb6b2210fc139155e0bec5a9dcd42c
khafidhkayyis <khafidhkayyis@gmail.com>
2026-08-14 10:24:05 +0700

    simple fatch

---
+++ src/app/users/page.tsx
@@ -0,0 +1,31 @@
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+};
+
+async function getUsers(): Promise<User[]> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    console.log('Response:', response);
+    if (!response.ok) {
+        throw new Error('Failed to fetch users');
+    }
+    return response.json();
+}
+
+export default async function UsersPage() {
+    const users = await getUsers();
+    return (
+        <main>
+            <h1>Users</h1>
+            {users.map((user) => (
+                <div key={user.id}>
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                    <p>{user.username}</p>
+                </div>
+            ))}
+        </main>
+    )
+}
---
+++ src/app/users/pages.md
@@ -0,0 +1,2 @@
+noted
+ketika ingin console pastikan harus ada code export default / code htmlnya 
