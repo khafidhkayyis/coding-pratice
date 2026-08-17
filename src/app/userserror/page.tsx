8f6738e12f3b8b6412fcb2e886b0a3b5c42e3229
khafidhkayyis <khafidhkayyis@gmail.com>
2026-08-17 10:47:21 +0700

    loading and error fatch api

---
+++ src/app/userserror/page.tsx
@@ -0,0 +1,37 @@
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+};
+
+async function getUsers(): Promise<User[]> {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch users');
+        }
+        return response.json()
+    } catch (error) {
+        console.log('Error:', error);
+        throw error;
+    }
+    return [];
+}
+
+export default async function UsersPage() {
+    const users = await getUsers();
+    return (
+        <main>
+            <h1>Users</h1>
+            {users.map((user) => (
+                <div key={user.id}>
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                    <p>{user.username}</p>
+                </div>
+            ))}
+        </main>
+    );
+}
