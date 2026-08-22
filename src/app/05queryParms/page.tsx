0a4207c3215daa88baa8e032f4c00517b23c072e
khafidhkayyis <khafidhkayyis@gmail.com>
2026-08-22 07:24:40 +0700

    queryparms

--- src/app/05queryParms/page.tsx
+++ src/app/05queryParms/page.tsx
@@ -5,12 +5,31 @@ type PageProps = {
     }>;
 };
 
+async function getUsers(page: string, limit: string) {
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${limit}`
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch users");
+    }
+
+    return response.json();
+}
+
 export default async function Page({ searchParams }: PageProps) {
     const { page, limit } = await searchParams;
+
+    const users = await getUsers(page, limit);
+
     return (
         <div>
             <p>Page: {page}</p>
-            <p>limit: {limit}</p>
+            <p>Limit: {limit}</p>
+
+            {users.map((user: any) => (
+                <p key={user.id}>{user.name}</p>
+            ))}
         </div>
-    )
+    );
 }
